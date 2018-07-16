ee23caee37423043bbc51e99a1d5881e43e011c1
Niko Barateli <nbara15@freeuni.edu.ge>
2018-07-16 16:55:46 +0400

    implemented /move

--- server.js
+++ server.js
@@ -23,98 +23,116 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 // Game state variables
-var board; 
+var board;
 var turn;
 
 // Reset the game
-var resetGame = function() {
-	board = [["", "", ""], ["", "", ""] ,["", "", ""]];
-	turn = "x";
+var resetGame = function () {
+    board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    turn = "x";
 };
 
 // Given a board, return true if the game has ended and false otherwise
-var gameEnded = function(board) { 
-	// Possible lines to check
-	var lines = [
-		[[0, 0, 0], [0, 1, 2]],
-		[[1, 1, 1], [0, 1, 2]],
-		[[2, 2, 2], [0, 1, 2]],
-		[[0, 1, 2], [0, 0, 0]],
-		[[0, 1, 2], [1, 1, 1]],
-		[[0, 1, 2], [2, 2, 2]],
-		[[0, 1, 2], [0, 1, 2]],
-		[[0, 1, 2], [2, 1, 0]]
-	];
+var gameEnded = function (board) {
+    // Possible lines to check
+    var lines = [
+        [[0, 0, 0], [0, 1, 2]],
+        [[1, 1, 1], [0, 1, 2]],
+        [[2, 2, 2], [0, 1, 2]],
+        [[0, 1, 2], [0, 0, 0]],
+        [[0, 1, 2], [1, 1, 1]],
+        [[0, 1, 2], [2, 2, 2]],
+        [[0, 1, 2], [0, 1, 2]],
+        [[0, 1, 2], [2, 1, 0]]
+    ];
 
-	// If any line is controlled by a single player, the game is ended
-	for (var i = 0; i < lines.length; i++) {
-		if (board[lines[i][0][0]][lines[i][1][0]]
-				== board[lines[i][0][1]][lines[i][1][1]]
-				&& board[lines[i][0][1]][lines[i][1][1]]
-				== board[lines[i][0][2]][lines[i][1][2]]
-				&& board[lines[i][0][0]][lines[i][1][0]] != "") {
-			return true;
-		}
-	}
+    // If any line is controlled by a single player, the game is ended
+    for (var i = 0; i < lines.length; i++) {
+        if (board[lines[i][0][0]][lines[i][1][0]]
+            == board[lines[i][0][1]][lines[i][1][1]]
+            && board[lines[i][0][1]][lines[i][1][1]]
+            == board[lines[i][0][2]][lines[i][1][2]]
+            && board[lines[i][0][0]][lines[i][1][0]] != "") {
+            return true;
+        }
+    }
 
-	// If all the spots are taken, the game has ended.
-	for (var row = 0; row < 3; row++) {
-		for (var col = 0; col < 3; col++) {
-			if (board[row][col] == "") {
-				return false;
-			}
-		}
-	}
-	return true;
+    // If all the spots are taken, the game has ended.
+    for (var row = 0; row < 3; row++) {
+        for (var col = 0; col < 3; col++) {
+            if (board[row][col] == "") {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 // Client page for Player X
-app.get("/playerx", function(req, res) {
-	res.render("client", {"player": "x"});
+app.get("/playerx", function (req, res) {
+    res.render("client", {"player": "x"});
 });
 
 // Client page for Player Y
-app.get("/playero", function(req, res) {
-	res.render("client", {"player": "o"});
+app.get("/playero", function (req, res) {
+    res.render("client", {"player": "o"});
 });
 
 // HTTP GET endpoint that resets the game
-app.get("/reset", function(req, res) {
-	resetGame();
-	res.send(JSON.stringify(true));
-	res.end();
+app.get("/reset", function (req, res) {
+    resetGame();
+    res.send(JSON.stringify(true));
+    res.end();
 });
 
 
 // HTTP GET endpoint that resets the game only if it is finished
-app.get("/newgame", function(req, res) {
-	if (gameEnded(board)) {
-		resetGame();
-		res.send(JSON.stringify(true));
-		res.end();
-	} else {
-		res.send(JSON.stringify(false));
-		res.end();
-	}
+app.get("/newgame", function (req, res) {
+    if (gameEnded(board)) {
+        resetGame();
+        res.send(JSON.stringify(true));
+        res.end();
+    } else {
+        res.send(JSON.stringify(false));
+        res.end();
+    }
 });
 
 // HTTP GET endpoint that gets the board
-app.get("/board", function(req, res) {
-	res.send(JSON.stringify(board));
-	res.end();
+app.get("/board", function (req, res) {
+    res.send(JSON.stringify(board));
+    res.end();
 });
 
 // HTTP GET endpoint that gets whose turn it is
-app.get("/turn", function(req, res) {
-	res.send(JSON.stringify(turn));
-	res.end();
+app.get("/turn", function (req, res) {
+    res.send(JSON.stringify(turn));
+    res.end();
 });
 
+function inRange(val, start, end) {
+    return start <= val && val <= end;
+}
+
+function valid(x, y, player) {
+
+    return (player === 'x' || player === 'o') && inRange(x, 0, board.length - 1)
+        && inRange(y, 0, board.length - 1) && board[x][y] === '' && turn === player;
+}
+
 // HTTP GET endpoint that makes a player move
-app.get("/move", function(req, res) {
-	// TODO: Implement this
-	res.send(JSON.stringify("Unimplemented"));
-	res.end();
+app.get("/move", function (req, res) {
+    // TODO: Implement this
+    let x = req.query.row, y = req.query.col, player = req.query.player;
+    if (valid(x, y, player)) {
+        board[x][y] = player
+        turn = gameEnded(board) ? "" : player === 'x' ? 'o' : 'x';
+        res.send(JSON.stringify(true));
+    } else {
+        res.send(JSON.stringify(false));
+    }
+
+    res.end();
 });
 
 // Initialize the game
